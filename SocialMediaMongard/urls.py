42e53c446709a5ba700a4515d113710d50d4136e
Ah_azhdari <dev.amirhossein.azhdari@outlook.com>
2022-04-26 12:07:01 +0430

    init files

---
+++ SocialMediaMongard/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls', namespace="home:home")),
+    path('accounts/', include('accounts.urls', namespace="accounts:accounts"))
+]
---
+++ accounts/views.py
@@ -0,0 +1,45 @@
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.models import User
+from accounts.forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth import authenticate, login
+
+
+class UserRegisterView(View):
+    form_class = UserRegistrationForm
+    template_name = 'accounts/register.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['user_name'], cd['email'], cd['password1'])
+            messages.success(request, 'you registered successfully', 'success')
+            return redirect('home:home')
+        return render(request, self.template_name, {"form": form})
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'accounts/login.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you logged in successfully', 'success')
+                return redirect('home:home')
+            messages.error(request, 'invalid username or password', 'warning')
+        return render(request, self.template_name, {"form": form})
