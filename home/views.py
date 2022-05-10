9c06f8311958b1d6c45f77833ccfc57588f1e80a
amirhossein azhdari <dev.amirhossein.azhdari@outlook.com>
2022-05-10 23:34:19 +0430

    use meta options for ordering model

--- home/views.py
+++ home/views.py
@@ -5,5 +5,5 @@ from posts.models import Post
 
 class HomeView(View):
     def get(self, request):
-        posts = Post.objects.order_by('-updated').all()
+        posts = Post.objects.all()
         return render(request, 'home/index.html', {"posts": posts})
