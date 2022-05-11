c157d6ea1b574616db7a31ccd2d9f48b56619afd
Ah_azhdari <dev.amirhossein.azhdari@outlook.com>
2022-05-11 14:53:56 +0430

    add email information

--- SocialMediaMongard/settings.py
+++ SocialMediaMongard/settings.py
@@ -9,12 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
-from dotenv import load_dotenv
 from pathlib import Path
-
-# load env files
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,8 +123,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('USER_EMAIL')  # read email address from .env file
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')  # get email password from .env file
+EMAIL_HOST_USER = 'amir.az3531@gmail.com'  # read email address from .env file
+EMAIL_HOST_PASSWORD = 'qrkgdvcwurlazgan'  # get email password from .env file
 # securer than SSL
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Question website'  # default string for all sent emails
