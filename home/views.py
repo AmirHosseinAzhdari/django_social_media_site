ef7ee28ac11e4f6ca573943742cfbf0640b105e5
AmirHosseinAzhdari <dev.amirhossein.azhdari@outlook.com>
2022-05-29 16:18:13 +0430

    add search form

--- home/views.py
+++ home/views.py
@@ -1,9 +1,18 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
+
+from home.forms import PostSearchForm
 from posts.models import Post
 
 
 class HomeView(View):
+    form_class = PostSearchForm
+
     def get(self, request):
-        posts = Post.objects.all()
-        return render(request, 'home/index.html', {"posts": posts})
+        if request.GET.get("search"):
+            search = request.GET.get("search")
+            posts = Post.objects.filter(Q(title__icontains=search) | Q(body__icontains=search))
+        else:
+            posts = Post.objects.all()
+        return render(request, 'home/index.html', {"posts": posts, "form": self.form_class})
